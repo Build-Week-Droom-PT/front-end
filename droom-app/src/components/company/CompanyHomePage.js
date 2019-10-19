837081573af9a6030353e34d29d11f68547f2e8e
Th5tch3r <amber.phan27@gmail.com>
2019-10-19 13:13:22 -0400

    added LogOut to remove token as anchor link in seekerhomepage and companyhomepage and the logout works

--- droom-app/src/components/company/CompanyHomePage.js
+++ droom-app/src/components/company/CompanyHomePage.js
@@ -1,8 +1,10 @@
 import React from "react";
+import LogOut from "../Forms/LogOut"
 
 export default function SeekerHomePage() {
     return (
         <div>
+            <a href="#" onClick={LogOut()}>Log Out</a>
             <h1>You are a company!</h1>
         </div>
     )
